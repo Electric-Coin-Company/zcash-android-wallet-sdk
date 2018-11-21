193a1f3b22a44284da8968f9844887b353fcfee1
Jack Grigg <jack@z.cash>
2018-11-21 13:51:37 +0000

    cargo fmt

--- src/main/rust/lib.rs
+++ src/main/rust/lib.rs
@@ -8,28 +8,35 @@ pub extern "C" fn test_response(_input: *const c_char) -> c_int {
 }
 
 /// JNI interface
-#[cfg(target_os="android")]
+#[cfg(target_os = "android")]
 #[allow(non_snake_case)]
 pub mod android {
     extern crate jni;
 
-    use super::*;
-    use self::jni::JNIEnv;
     use self::jni::objects::{JClass, JString};
     use self::jni::sys::*;
+    use self::jni::JNIEnv;
+    use super::*;
 
     #[no_mangle]
-    pub unsafe extern "C" fn Java_cash_z_wallet_sdk_jni_JniConverter_getMagicInt(env: JNIEnv, _: JClass, test_input: JString) -> jint {
-        let jvm_text = env.get_string(test_input).expect("unable to find text for test input");
+    pub unsafe extern "C" fn Java_cash_z_wallet_sdk_jni_JniConverter_getMagicInt(
+        env: JNIEnv,
+        _: JClass,
+        test_input: JString,
+    ) -> jint {
+        let jvm_text = env
+            .get_string(test_input)
+            .expect("unable to find text for test input");
         test_response(jvm_text.as_ptr())
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn Java_cash_z_wallet_sdk_jni_JniConverter_sendComplexData(env: JNIEnv, _: JClass, wallet_data: jbyteArray) -> jint {
+    pub unsafe extern "C" fn Java_cash_z_wallet_sdk_jni_JniConverter_sendComplexData(
+        env: JNIEnv,
+        _: JClass,
+        wallet_data: jbyteArray,
+    ) -> jint {
         let bytes = env.convert_byte_array(wallet_data);
         bytes.unwrap().len() as i32
     }
 }
-
-
-
