f41e02d1bedc84a28867516fc906fbfb6de78b15
Kevin Gorham <anothergmale@gmail.com>
2018-11-28 12:20:11 -0500

    test scan blocks

--- src/androidTest/java/cash/z/wallet/sdk/jni/JniConverterTest.kt
+++ src/androidTest/java/cash/z/wallet/sdk/jni/JniConverterTest.kt
@@ -2,11 +2,17 @@ package cash.z.wallet.sdk.jni
 
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
 
 class JniConverterTest {
 
-    val converter:JniConverter = JniConverter()
+    val converter: JniConverter = JniConverter()
+
+    @Before
+    fun initLogs() {
+        converter.initLogs()
+    }
 
     @Test
     fun testGetAddress_exists() {
@@ -18,6 +24,18 @@ class JniConverterTest {
         val address = converter.getAddress("dummyseed".toByteArray())
         val expectedAddress = "ztestsapling1snmqdnfqnc407pvqw7sld8w5zxx6nd0523kvlj4jf39uvxvh7vn0hs3q38n07806dwwecqwke3t"
         assertEquals("Invalid address", expectedAddress, address)
+    }
+
+    @Test
+    fun testScanBlocks() {
+        val result = converter.scanBlocks(
+            "/data/user/0/cash.z.wallet.sdk.test/databases/dummy-cache.db",
+            343900,
+            354446,
+            "dummyseed".toByteArray()
+        )
+        val expectedAddress = "ztestsapling1snmqdnfqnc407pvqw7sld8w5zxx6nd0523kvlj4jf39uvxvh7vn0hs3q38n07806dwwecqwke3t"
+        assertEquals(5, result.size)
     }
 
 }
