58f5a24a9088a22d127351977b09911204691d54
Kevin Gorham <anothergmale@gmail.com>
2018-11-28 01:43:36 -0500

    checkpoint. database exists

--- src/androidTest/java/cash/z/wallet/sdk/dao/ComplactBlockDaoTest.kt
+++ src/androidTest/java/cash/z/wallet/sdk/dao/ComplactBlockDaoTest.kt
@@ -1,14 +1,36 @@
 package cash.z.wallet.sdk.dao
 
-import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+import cash.z.wallet.sdk.db.CompactBlockDb
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class ComplactBlockDaoTest {
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     private var dao: CompactBlockDao? = null
+    private var db: CompactBlockDb? = null
+
+    @Before
+    fun initDb() {
+        db = Room.inMemoryDatabaseBuilder(
+            InstrumentationRegistry.getInstrumentation().context,
+            CompactBlockDb::class.java
+        )
+            .build()
+            // .apply { dao = complactBlockDao() }
+    }
+
+    @Test
+    fun testDbExists() {
+        assertNotNull(db)
+    }
 
     @Test
     fun testDaoExists() {
