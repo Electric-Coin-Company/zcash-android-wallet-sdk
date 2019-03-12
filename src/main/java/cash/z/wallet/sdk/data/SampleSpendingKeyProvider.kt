c7d85f2a510df04c62e42cf11e35ef9ace05f108
Kevin Gorham <anothergmale@gmail.com>
2019-03-12 13:05:38 -0400

    Cleanup
    This commit will get iteratively polished until the PR is ready
    - Addresses PR feedback https://github.com/zcash/zcash-android-wallet-sdk/pull/16#discussion_r264948336

--- src/main/java/cash/z/wallet/sdk/data/SampleSpendingKeyProvider.kt
+++ src/main/java/cash/z/wallet/sdk/data/SampleSpendingKeyProvider.kt
@@ -10,7 +10,7 @@ class SampleSpendingKeyProvider(private val seedValue: String) : ReadWriteProper
     }
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        // dynamically generating keyes, based on seed is out of scope for this sample
+        // dynamically generating keys, based on seed is out of scope for this sample
         if(seedValue != "dummyseed") throw IllegalStateException("This sample key provider only supports the dummy seed")
         return "secret-extended-key-test1q0f0urnmqqqqpqxlree5urprcmg9pdgvr2c88qhm862etv65eu84r9zwannpz4g88299xyhv7wf9xkecag653jlwwwyxrymfraqsnz8qfgds70qjammscxxyl7s7p9xz9w906epdpy8ztsjd7ez7phcd5vj7syx68sjskqs8j9lef2uuacghsh8puuvsy9u25pfvcdznta33qe6xh5lrlnhdkgymnpdug4jm6tpf803cad6tqa9c0ewq9l03fqxatevm97jmuv8u0ccxjews5"
     }
