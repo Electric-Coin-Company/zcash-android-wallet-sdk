4e2065e0dffaaaf138a4d6b8d94e52cc332c70fc
Kevin Gorham <kevin.gorham@electriccoin.co>
2020-10-30 09:20:32 -0400

    Add ability to erase by alias.

--- src/androidTest/java/cash/z/ecc/android/sdk/InitializerTest.kt
+++ src/androidTest/java/cash/z/ecc/android/sdk/InitializerTest.kt
@@ -3,22 +3,37 @@ package cash.z.ecc.android.sdk
 import androidx.test.platform.app.InstrumentationRegistry
 import cash.z.ecc.android.sdk.ext.TroubleshootingTwig
 import cash.z.ecc.android.sdk.ext.Twig
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 class InitializerTest {
 
     @Test
     fun testInit() {
-        val height = 1_419_900
+        val height = 980000
 
         val initializer = Initializer(context) { config ->
             config.importedWalletBirthday(height)
             config.setViewingKeys("zxviews1qvn6j50dqqqqpqxqkvqgx2sp63jccr4k5t8zefadpzsu0yy73vczfznwc794xz6lvy3yp5ucv43lww48zz95ey5vhrsq83dqh0ky9junq0cww2wjp9c3cd45n5l5x8l2g9atnx27e9jgyy8zasjy26gugjtefphan9al3tx208m8ekev5kkx3ug6pd0qk4gq4j4wfuxajn388pfpq54wklwktqkyjz9e6gam0n09xjc35ncd3yah5aa9ezj55lk4u7v7hn0v86vz7ygq4qj2v",
             "zxviews1qv886f6hqqqqpqy2ajg9sm22vs4gm4hhajthctfkfws34u45pjtut3qmz0eatpqzvllgsvlk3x0y35ktx5fnzqqzueyph20k3328kx46y3u5xs4750cwuwjuuccfp7la6rh8yt2vjz6tylsrwzy3khtjjzw7etkae6gw3vq608k7quka4nxkeqdxxsr9xxdagv2rhhwugs6w0cquu2ykgzgaln2vyv6ah3ram2h6lrpxuznyczt2xl3lyxcwlk4wfz5rh7wzfd7642c2ae5d7")
-            config.alias = "VkInitTest2"
+            config.alias = "VkInitTest1"
         }
         assertEquals(height, initializer.birthday.height)
+        initializer.erase()
+    }
+
+    @Test
+    fun testErase() {
+        val alias = "VkInitTest2"
+        Initializer(context) { config ->
+            config.importedWalletBirthday(1_419_900)
+            config.setViewingKeys("zxviews1qvn6j50dqqqqpqxqkvqgx2sp63jccr4k5t8zefadpzsu0yy73vczfznwc794xz6lvy3yp5ucv43lww48zz95ey5vhrsq83dqh0ky9junq0cww2wjp9c3cd45n5l5x8l2g9atnx27e9jgyy8zasjy26gugjtefphan9al3tx208m8ekev5kkx3ug6pd0qk4gq4j4wfuxajn388pfpq54wklwktqkyjz9e6gam0n09xjc35ncd3yah5aa9ezj55lk4u7v7hn0v86vz7ygq4qj2v",
+            "zxviews1qv886f6hqqqqpqy2ajg9sm22vs4gm4hhajthctfkfws34u45pjtut3qmz0eatpqzvllgsvlk3x0y35ktx5fnzqqzueyph20k3328kx46y3u5xs4750cwuwjuuccfp7la6rh8yt2vjz6tylsrwzy3khtjjzw7etkae6gw3vq608k7quka4nxkeqdxxsr9xxdagv2rhhwugs6w0cquu2ykgzgaln2vyv6ah3ram2h6lrpxuznyczt2xl3lyxcwlk4wfz5rh7wzfd7642c2ae5d7")
+            config.alias = alias
+        }
+
+        assertFalse("Expected false when erasing nothing.", Initializer.erase(context))
+        assertTrue("Failed to erase initializer", Initializer.erase(context, alias))
     }
 
     companion object {
