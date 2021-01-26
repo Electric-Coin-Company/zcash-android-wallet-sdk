863cbca93bdb92d3ae7c5f94a5899b14fbb9012e
Eljo <elioprifti@gmail.com>
2021-01-26 21:44:30 +0100

    apply changes on PR request

--- src/androidTest/java/cash/z/ecc/android/sdk/tool/SaplingParamToolTest.kt
+++ src/androidTest/java/cash/z/ecc/android/sdk/tool/SaplingParamToolTest.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import cash.z.ecc.android.sdk.exception.LightWalletException
+import cash.z.ecc.android.sdk.ext.ZcashSdk
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 class SaplingParamToolTest {
@@ -17,30 +20,54 @@ class SaplingParamToolTest {
 
     val cacheDir = "${context.cacheDir.absolutePath}/params"
 
+    @Before
+    fun setup() {
+        // clear the param files
+        SaplingParamTool.clear(cacheDir)
+    }
+
 
     @Test
-    fun testFilesExist(){
+    suspend fun testFilesExists(){
+        // Given
+        SaplingParamTool.fetchParams(cacheDir)
+
+        // When
         val result = SaplingParamTool.validate(cacheDir)
 
+        // Then
         Assert.assertFalse(result)
     }
 
     @Test
-    fun testOnlySpendFileExit(){
+    suspend fun testOnlySpendFileExits(){
+        // Given
+        SaplingParamTool.fetchParams(cacheDir)
+        File("$cacheDir/${ZcashSdk.OUTPUT_PARAM_FILE_NAME}").delete()
+
+        // When
         val result = SaplingParamTool.validate(cacheDir)
 
-        Assert.assertFalse(result)
+        // Then
+        Assert.assertFalse("Validation should fail when the spend params are missing", result)
     }
 
     @Test
-    fun testOnlyOutputOFileExit(){
+    suspend fun testOnlyOutputOFileExits(){
+        // Given
+        SaplingParamTool.fetchParams(cacheDir)
+        File("$cacheDir/${ZcashSdk.SPEND_PARAM_FILE_NAME}").delete()
+
+        // When
         val result = SaplingParamTool.validate(cacheDir)
 
-        Assert.assertFalse(result)
+        // Then
+        Assert.assertFalse("Validation should fail when the spend params are missing", result)
     }
 
     @Test
     suspend fun testInsufficientDeviceStorage() = runBlocking {
+        // Given
         SaplingParamTool.fetchParams(cacheDir)
 
         Assert.assertFalse("insufficient storage",false)
