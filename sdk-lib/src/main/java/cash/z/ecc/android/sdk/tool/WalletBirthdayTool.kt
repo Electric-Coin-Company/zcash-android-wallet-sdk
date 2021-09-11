a58a235d2ccfae53462b44930792bf5b0591c2f2
Carter Jernigan <git@carterjernigan.com>
2021-09-11 10:38:52 -0400

    [#270] Add regression tests for assets

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/tool/WalletBirthdayTool.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/tool/WalletBirthdayTool.kt
@@ -1,15 +1,16 @@
 package cash.z.ecc.android.sdk.tool
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import cash.z.ecc.android.sdk.exception.BirthdayException
 import cash.z.ecc.android.sdk.ext.twig
 import cash.z.ecc.android.sdk.type.WalletBirthday
 import cash.z.ecc.android.sdk.type.ZcashNetwork
 import com.google.gson.Gson
 import com.google.gson.stream.JsonReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.util.Arrays
-import java.util.Locale
 
 /**
  * Tool for loading checkpoints for the wallet, based on the height at which the wallet was born.
@@ -17,7 +18,7 @@ import java.util.Locale
  * @param appContext needed for loading checkpoints from the app's assets directory.
  */
 class WalletBirthdayTool(appContext: Context) {
-    val context = appContext.applicationContext
+    private val context = appContext.applicationContext
 
     /**
      * Load the nearest checkpoint to the given birthday height. If null is given, then this
@@ -51,13 +52,23 @@ class WalletBirthdayTool(appContext: Context) {
                     )
             }
 
+        // TODO: This method performs disk IO; convert to suspending function
+        // Converting this to suspending will then propagate
+        @Throws(IOException::class)
+        internal fun listBirthdayDirectoryContents(context: Context, directory: String) =
+            context.assets.list(directory)
+
         /**
          * Returns the directory within the assets folder where birthday data
          * (i.e. sapling trees for a given height) can be found.
          */
-        private fun birthdayDirectory(network: ZcashNetwork): String {
-            return "saplingtree/${network.networkName.toLowerCase(Locale.US)}"
-        }
+        @VisibleForTesting
+        internal fun birthdayDirectory(network: ZcashNetwork) =
+            "saplingtree/${network.networkName.lowercase()}"
+
+        internal fun birthdayHeight(fileName: String) = fileName.split('.').first().toInt()
+
+        private fun Array<String>.sortDescending() = apply { sortByDescending { birthdayHeight(it) } }
 
         /**
          * Load the given birthday file from the assets of the given context. When no height is
@@ -76,16 +87,7 @@ class WalletBirthdayTool(appContext: Context) {
         ): WalletBirthday {
             twig("loading birthday from assets: $birthdayHeight")
             val directory = birthdayDirectory(network)
-            val treeFiles =
-                context.assets.list(directory)?.apply {
-                    sortByDescending { fileName ->
-                        try {
-                            fileName.split('.').first().toInt()
-                        } catch (t: Throwable) {
-                            network.saplingActivationHeight
-                        }
-                    }
-                }
+            val treeFiles = listBirthdayDirectoryContents(context, directory)?.sortDescending()
             if (treeFiles.isNullOrEmpty()) throw BirthdayException.MissingBirthdayFilesException(
                 directory
             )
@@ -94,7 +96,7 @@ class WalletBirthdayTool(appContext: Context) {
             try {
                 file = if (birthdayHeight == null) treeFiles.first() else {
                     treeFiles.first {
-                        it.split(".").first().toInt() <= birthdayHeight
+                        birthdayHeight(it) <= birthdayHeight
                     }
                 }
             } catch (t: Throwable) {
