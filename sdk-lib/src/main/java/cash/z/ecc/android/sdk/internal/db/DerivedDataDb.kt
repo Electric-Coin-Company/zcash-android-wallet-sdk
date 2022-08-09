9efa13335f42bd7c99f719457b9f1304908eb89e
Honza <rychnovsky.honza@gmail.com>
2022-08-09 15:50:20 +0200

    Remove redundant initializer
    - Fixed warning: Variable 'hasFinalMatch' initializer is redundant.

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/db/DerivedDataDb.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/db/DerivedDataDb.kt
@@ -433,7 +433,6 @@ interface TransactionDao {
         var success = false
         try {
             var hasInitialMatch = false
-            var hasFinalMatch = true
             twig("[cleanup] cleanupCancelledTx starting...")
             findUnminedTransactionIds(rawTransactionId).also {
                 twig("[cleanup] cleanupCancelledTx found ${it.size} matching transactions to cleanup")
@@ -441,7 +440,7 @@ interface TransactionDao {
                 hasInitialMatch = true
                 removeInvalidOutboundTransaction(transactionId)
             }
-            hasFinalMatch = findMatchingTransactionId(rawTransactionId) != null
+            val hasFinalMatch = findMatchingTransactionId(rawTransactionId) != null
             success = hasInitialMatch && !hasFinalMatch
             twig("[cleanup] cleanupCancelledTx Done. success? $success")
         } catch (t: Throwable) {
