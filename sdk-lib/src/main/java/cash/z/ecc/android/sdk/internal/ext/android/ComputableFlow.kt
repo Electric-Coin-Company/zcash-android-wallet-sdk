7903adf93002d0d1b8797a9fdb966a644f8757e2
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-08-16 15:22:05 +0200

    Remove ConflatedBroadcastChannel from ComputableFlow

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/android/ComputableFlow.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/android/ComputableFlow.kt
@@ -3,37 +3,29 @@ package cash.z.ecc.android.sdk.internal.ext.android
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.channels.ConflatedBroadcastChannel
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 /* Adapted from ComputableLiveData */
-// TODO [#658] https://github.com/zcash/zcash-android-wallet-sdk/issues/658
-@Suppress("DEPRECATION")
-@OptIn(ObsoleteCoroutinesApi::class)
 abstract class ComputableFlow<T>(dispatcher: CoroutineDispatcher = Dispatchers.IO) {
     private val computationScope: CoroutineScope = CoroutineScope(dispatcher + SupervisorJob())
-    private val computationChannel: ConflatedBroadcastChannel<T> = ConflatedBroadcastChannel()
-    internal val flow = computationChannel.asFlow().flowOn(dispatcher).onStart {
-        invalidate()
-    }
+    private val computationFlow: MutableSharedFlow<T> = MutableSharedFlow(replay = 1)
+    internal val flow = computationFlow.asSharedFlow()
 
     /**
      * Invalidates the flow.
      * This will trigger a call to [.compute].
      */
     fun invalidate() {
-        computationScope.launch { computationChannel.send(compute()) }
+        computationScope.launch { computationFlow.emit(compute()) }
     }
 
     fun cancel() {
         computationScope.cancel()
-        computationChannel.cancel()
+        computationFlow.resetReplayCache()
     }
 
     protected abstract fun compute(): T
