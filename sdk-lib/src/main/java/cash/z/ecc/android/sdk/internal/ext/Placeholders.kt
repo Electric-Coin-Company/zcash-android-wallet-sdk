88542c6009f4aa09a24c0a4c7ded76819872001d
Honza <rychnovsky.honza@gmail.com>
2022-08-19 14:45:22 +0200

    Use error() instead of throwing an IllegalStateException
    - Fixed warnings:
    - Placeholders.kt:21:39: Use check() or error() instead of throwing an IllegalStateException. [UseCheckOrError]
    - FlowPagedListBuilder.kt:89:21: Use check() or error() instead of throwing an IllegalStateException. [UseCheckOrError]

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/Placeholders.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/Placeholders.kt
@@ -18,7 +18,9 @@ class SampleSpendingKeyProvider(private val seedValue: String) : ReadWriteProper
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): String {
         // dynamically generating keys, based on seed is out of scope for this sample
-        if (seedValue != "dummyseed") throw IllegalStateException("This sample provider only supports the dummy seed")
+        if (seedValue != "dummyseed") {
+            error("This sample provider only supports the dummy seed")
+        }
         return "secret-extended-key-test1q0f0urnmqqqqpqxlree5urprcmg9pdgvr2c88qhm862etv65eu84r9zwannpz4g88299xyhv7wf9" +
             "xkecag653jlwwwyxrymfraqsnz8qfgds70qjammscxxyl7s7p9xz9w906epdpy8ztsjd7ez7phcd5vj7syx68sjskqs8j9lef2uu" +
             "acghsh8puuvsy9u25pfvcdznta33qe6xh5lrlnhdkgymnpdug4jm6tpf803cad6tqa9c0ewq9l03fqxatevm97jmuv8u0ccxjews5"
