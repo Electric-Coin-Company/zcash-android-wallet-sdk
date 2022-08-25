8fd2f17b27f1c207b0c250a6fb20521b82702d91
Carter Jernigan <git@carterjernigan.com>
2022-08-25 07:38:53 -0400

    Optimize readFileLinesInFlow

--- sdk-lib/src/androidTest/java/cash/z/ecc/android/sdk/test/Global.kt
+++ sdk-lib/src/androidTest/java/cash/z/ecc/android/sdk/test/Global.kt
@@ -3,25 +3,23 @@ package cash.z.ecc.android.sdk.test
 import android.content.Context
 import androidx.annotation.StringRes
 import androidx.test.core.app.ApplicationProvider
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlin.test.assertNotNull
 
 fun getAppContext(): Context = ApplicationProvider.getApplicationContext()
 
 fun getStringResource(@StringRes resId: Int) = getAppContext().getString(resId)
 
-fun getStringResourceWithArgs(@StringRes resId: Int, vararg formatArgs: String) = getAppContext().getString(resId, *formatArgs)
+fun getStringResourceWithArgs(@StringRes resId: Int, vararg formatArgs: String) =
+    getAppContext().getString(resId, *formatArgs)
 
-fun readFileLinesInFlow(filePathName: String) = callbackFlow {
+fun readFileLinesInFlow(filePathName: String) = flow<String> {
     val testFile = javaClass.getResourceAsStream(filePathName)
     assertNotNull(testFile, "Test file read failure.")
 
-    val reader = testFile.bufferedReader()
-    reader.forEachLine { line ->
-        trySend(line)
-    }.also {
-        close()
-    }
-    awaitClose { /* nothing to close here */ }
-}
+    emitAll(testFile.bufferedReader().lineSequence().asFlow())
+}.flowOn(Dispatchers.IO)
