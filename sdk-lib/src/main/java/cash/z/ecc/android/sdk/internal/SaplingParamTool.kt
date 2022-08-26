5628470a9551994a944cd61d36a31e24191e960c
Honza <rychnovsky.honza@gmail.com>
2022-08-26 13:54:48 +0200

    Optimization of sapling files download
    - Now we download only those sapling files, which are missing.
    - Created or changed related tests to cover this new behaviour (dedicated fixture included).
    - Improved code documentation.

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/SaplingParamTool.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/SaplingParamTool.kt
@@ -32,91 +32,95 @@ object SaplingParamTool {
     internal const val OUTPUT_PARAM_FILE_MAX_BYTES_SIZE = 5L * 1024L * 1024L
 
     /**
-     * Checks the given directory for the output and spending params and calls [fetchParams] if
-     * they're missing.
+     * Checks the given directory for the output and spending params and calls [fetchParams] for those, which are
+     * missing.
      *
      * @param destinationDir the directory where the params should be stored.
+     *
+     * @throws TransactionEncoderException.MissingParamsException in case of failure while checking sapling params
+     * files
      */
+    @Throws(TransactionEncoderException.MissingParamsException::class)
     suspend fun ensureParams(destinationDir: String) {
-        var hadError = false
         arrayOf(
-            ZcashSdk.SPEND_PARAM_FILE_NAME,
-            ZcashSdk.OUTPUT_PARAM_FILE_NAME
-        ).forEach { paramFileName ->
-            if (!File(destinationDir, paramFileName).existsSuspend()) {
-                twig("WARNING: $paramFileName not found at location: $destinationDir")
-                hadError = true
-            }
-        }
-        if (hadError) {
-            @Suppress("TooGenericExceptionCaught")
+            SaplingFileParameters(
+                destinationDir,
+                ZcashSdk.SPEND_PARAM_FILE_NAME,
+                SPEND_PARAM_FILE_MAX_BYTES_SIZE
+            ),
+            SaplingFileParameters(
+                destinationDir,
+                ZcashSdk.OUTPUT_PARAM_FILE_NAME,
+                OUTPUT_PARAM_FILE_MAX_BYTES_SIZE
+            )
+        ).filter {
+            !File(it.destinationDirectoryPath, it.fileName).existsSuspend()
+        }.forEach {
             try {
-                Bush.trunk.twigTask("attempting to download missing params") {
-                    fetchParams(destinationDir)
-                }
-            } catch (e: Throwable) {
-                twig("failed to fetch params due to: $e")
+                twig("Attempting to download missing params: ${it.fileName}.")
+                fetchParams(it)
+            } catch (e: TransactionEncoderException.FetchParamsException) {
+                twig("Failed to fetch params ${it.fileName} due to: $e")
                 throw TransactionEncoderException.MissingParamsException
             }
+        }
+
+        if (!validate(destinationDir)) {
+            twig("Fetching sapling params files failed.")
+            throw TransactionEncoderException.MissingParamsException
         }
     }
 
     /**
-     * Download and store the params into the given directory.
+     * Download and store the params file into the given directory. It also checks the file size to eliminate the
+     * risk of downloading potentially large file from a malicious server.
      *
-     * @param destinationDir the directory where the params will be stored. It's assumed that we
-     * have write access to this directory. Typically, this should be the app's cache directory
-     * because it is not harmful if these files are cleared by the user since they are downloaded
-     * on-demand.
+     * @param paramsToFetch parameters wrapper class, which holds information about it
+     *
+     * @throws TransactionEncoderException.FetchParamsException if any error while downloading the params file occurs
      */
-    suspend fun fetchParams(destinationDir: String) {
-        var failureMessage = ""
-        arrayOf(
-            SaplingFileParameters(ZcashSdk.SPEND_PARAM_FILE_NAME, SPEND_PARAM_FILE_MAX_BYTES_SIZE),
-            SaplingFileParameters(ZcashSdk.OUTPUT_PARAM_FILE_NAME, OUTPUT_PARAM_FILE_MAX_BYTES_SIZE)
-        ).forEach { parameters ->
-            val url = URL("${ZcashSdk.CLOUD_PARAM_DIR_URL}/${parameters.filePathName}")
+    @Throws(TransactionEncoderException.FetchParamsException::class)
+    internal suspend fun fetchParams(paramsToFetch: SaplingFileParameters) {
+        val url = URL("${ZcashSdk.CLOUD_PARAM_DIR_URL}/${paramsToFetch.fileName}")
 
-            val file = File(destinationDir, parameters.filePathName)
-            if (file.parentFile?.existsSuspend() == true) {
-                twig("Directory ${file.parentFile?.name} exists!")
-            } else {
-                twig("Directory did not exist attempting to make it.")
-                file.parentFile?.mkdirsSuspend()
-            }
+        val file = File(paramsToFetch.destinationDirectoryPath, paramsToFetch.fileName)
+        if (file.parentFile?.existsSuspend() == true) {
+            twig("Directory ${file.parentFile?.name} exists!")
+        } else {
+            twig("Directory did not exist attempting to make it.")
+            file.parentFile?.mkdirsSuspend()
+        }
 
-            withContext(Dispatchers.IO) {
-                runCatching {
-                    Channels.newChannel(url.openStream()).use { readableByteChannel ->
-                        file.outputStream().use { fileOutputStream ->
-                            fileOutputStream.channel.use { fileChannel ->
-                                // Transfers bytes from stream to file from position 0 to end position or to max
-                                // file size limit. This eliminates the risk of downloading potentially large files
-                                // from a malicious server. We need to make a check of the file hash then.
-                                fileChannel.transferFrom(readableByteChannel, 0, parameters.fileMaxSize)
-                            }
+        withContext(Dispatchers.IO) {
+            runCatching {
+                Channels.newChannel(url.openStream()).use { readableByteChannel ->
+                    file.outputStream().use { fileOutputStream ->
+                        fileOutputStream.channel.use { fileChannel ->
+                            // Transfers bytes from stream to file from position 0 to end position or to max
+                            // file size limit. This eliminates the risk of downloading potentially large files
+                            // from a malicious server. We need to make a check of the file hash then.
+                            fileChannel.transferFrom(readableByteChannel, 0, paramsToFetch.fileMaxSize)
                         }
                     }
-                }.onFailure { exception ->
-                    // IllegalArgumentException - If the preconditions on the parameters do not hold
-                    // NonReadableChannelException - If the source channel was not opened for reading
-                    // NonWritableChannelException - If this channel was not opened for writing
-                    // ClosedChannelException - If either this channel or the source channel is closed
-                    // AsynchronousCloseException - If another thread closes either channel while the transfer is
-                    // in progress
-                    // ClosedByInterruptException - If another thread interrupts the current thread while the
-                    // transfer is in progress, thereby closing both channels and setting the current thread's
-                    // interrupt status
-                    // IOException - If some other I/O error occurs
-                    failureMessage += "Error while fetching ${parameters.filePathName}, caused by $exception\n"
-                    twig(failureMessage)
-                }.onSuccess {
-                    twig("Fetch and write of ${parameters.filePathName} succeeded.")
                 }
+            }.onFailure { exception ->
+                // IllegalArgumentException - If the preconditions on the parameters do not hold
+                // NonReadableChannelException - If the source channel was not opened for reading
+                // NonWritableChannelException - If this channel was not opened for writing
+                // ClosedChannelException - If either this channel or the source channel is closed
+                // AsynchronousCloseException - If another thread closes either channel while the transfer is
+                // in progress
+                // ClosedByInterruptException - If another thread interrupts the current thread while the
+                // transfer is in progress, thereby closing both channels and setting the current thread's
+                // interrupt status
+                // IOException - If some other I/O error occurs
+                "Error while fetching ${paramsToFetch.fileName}, caused by $exception".also {
+                    twig(it)
+                    throw TransactionEncoderException.FetchParamsException(it)
+                }
+            }.onSuccess {
+                twig("Fetch and write of ${paramsToFetch.fileName} succeeded.")
             }
-        }
-        if (failureMessage.isNotEmpty()) {
-            throw TransactionEncoderException.FetchParamsException(failureMessage)
         }
     }
 
@@ -143,7 +147,7 @@ object SaplingParamTool {
         ).all { paramFileName ->
             File(destinationDir, paramFileName).existsSuspend()
         }.also {
-            println("Param files${if (!it) "did not" else ""} both exist!")
+            println("Param files ${if (!it) "did not" else ""} both exist!")
         }
     }
 }
@@ -152,6 +156,7 @@ object SaplingParamTool {
  * Sapling file parameters class to hold each sapling file attributes.
  */
 internal data class SaplingFileParameters(
-    val filePathName: String,
+    val destinationDirectoryPath: String,
+    val fileName: String,
     val fileMaxSize: Long
 )
