fc3ac2720544a9e8919efb582fb14a59d8922266
Honza <rychnovsky.honza@gmail.com>
2023-01-24 14:17:10 +0100

    Update unsafe suffix documentation

--- lightwallet-client-lib/src/main/java/co/electriccoin/lightwallet/client/model/LightWalletEndpointInfoUnsafe.kt
+++ lightwallet-client-lib/src/main/java/co/electriccoin/lightwallet/client/model/LightWalletEndpointInfoUnsafe.kt
@@ -2,6 +2,11 @@ package co.electriccoin.lightwallet.client.model
 
 import cash.z.wallet.sdk.internal.rpc.Service
 
+/**
+ * A lightwalletd endpoint information, which has come from the Light Wallet server.
+ *
+ * It is marked as "unsafe" because it is not guaranteed to be valid.
+ */
 data class LightWalletEndpointInfoUnsafe(
     val chainName: String,
     val consensusBranchId: String,
--- lightwallet-client-lib/src/main/java/co/electriccoin/lightwallet/client/model/RawTransactionUnsafe.kt
+++ lightwallet-client-lib/src/main/java/co/electriccoin/lightwallet/client/model/RawTransactionUnsafe.kt
@@ -2,6 +2,11 @@ package co.electriccoin.lightwallet.client.model
 
 import cash.z.wallet.sdk.internal.rpc.Service.RawTransaction
 
+/**
+ * RawTransaction contains the complete transaction data, which has come from the Light Wallet server.
+ *
+ * It is marked as "unsafe" because it is not guaranteed to be valid.
+ */
 class RawTransactionUnsafe(val height: BlockHeightUnsafe, val data: ByteArray) {
     companion object {
         fun new(rawTransaction: RawTransaction) = RawTransactionUnsafe(
