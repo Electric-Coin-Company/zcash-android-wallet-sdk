b422fea25c5afe90a21e6dfcf0344a1ce985545e
Honza <rychnovsky.honza@gmail.com>
2023-03-02 15:04:49 +0100

    Check and document JniBlockMeta params ranges

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/JniBlockMeta.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/JniBlockMeta.kt
@@ -4,6 +4,15 @@ import androidx.annotation.Keep
 import cash.z.ecc.android.sdk.internal.storage.block.CompactBlockOutputsCounts
 import cash.z.wallet.sdk.internal.rpc.CompactFormats.CompactBlock
 
+/**
+ * Serves as cross layer (Kotlin, Rust) communication class.
+ *
+ * @param height the block's height - although it's type Long, it needs to be in UInt range
+ * @param hash the block's hash (ID of the block)
+ * @param time the block's time. Unix epoch time when the block was mined.
+ * @param saplingOutputsCount the sapling outputs count - although its type is Long, it needs to be in UInt range
+ * @param orchardOutputsCount the orchard outputs count - although its type is Long, it needs to be in UInt range
+ */
 @Keep
 class JniBlockMeta(
     val height: Long,
@@ -12,7 +21,23 @@ class JniBlockMeta(
     val saplingOutputsCount: Long,
     val orchardOutputsCount: Long
 ) {
+    init {
+        // We require some of the parameters below to be in the range of unsigned integer, because of the Rust layer
+        // implementation.
+        require(UINT_RANGE.contains(height)) {
+            "Height $height is outside of allowed range $UINT_RANGE"
+        }
+        require(UINT_RANGE.contains(saplingOutputsCount)) {
+            "SaplingOutputsCount $saplingOutputsCount is outside of allowed range $UINT_RANGE"
+        }
+        require(UINT_RANGE.contains(orchardOutputsCount)) {
+            "SaplingOutputsCount $orchardOutputsCount is outside of allowed range $UINT_RANGE"
+        }
+    }
+
     companion object {
+        private val UINT_RANGE = 0.toLong()..UInt.MAX_VALUE.toLong()
+
         internal fun new(block: CompactBlock, outputs: CompactBlockOutputsCounts): JniBlockMeta {
             return JniBlockMeta(
                 height = block.height,
