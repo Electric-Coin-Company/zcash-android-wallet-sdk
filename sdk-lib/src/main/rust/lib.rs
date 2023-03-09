8dd9c387dc75edff23493bb36bc08ba79a364401
Honza <rychnovsky.honza@gmail.com>
2023-03-09 10:03:35 +0100

    Finish lib.rs code review comments applying

--- sdk-lib/src/main/rust/lib.rs
+++ sdk-lib/src/main/rust/lib.rs
@@ -1509,8 +1509,8 @@ pub unsafe extern "C" fn Java_cash_z_ecc_android_sdk_jni_RustBackend_listTranspa
 ) -> jobjectArray {
     let res = panic::catch_unwind(|| {
         let network = parse_network(network_id as u32)?;
-        // Valid value of the Option<Network> is guaranteed by the previous network_id parsing
-        let zcash_network = network.address_network().expect("network_id parsing should not have resulted in an unrecognized network type.");
+        let zcash_network = network.address_network()
+            .expect("network_id parsing should not have resulted in an unrecognized network type.");
         let db_data = wallet_db(&env, network, db_data)?;
         let account_id = if account_id >= 0 {
             account_id as u32
@@ -1529,7 +1529,7 @@ pub unsafe extern "C" fn Java_cash_z_ecc_android_sdk_jni_RustBackend_listTranspa
                                 ZcashAddress::from_transparent_p2pkh(zcash_network, *data)
                             }
                             TransparentAddress::Script(data) => {
-                                ZcashAddress::from_transparent_p2sh(zcash_network.unwrap(), *data)
+                                ZcashAddress::from_transparent_p2sh(zcash_network, *data)
                             }
                         };
                         taddr.encode()
