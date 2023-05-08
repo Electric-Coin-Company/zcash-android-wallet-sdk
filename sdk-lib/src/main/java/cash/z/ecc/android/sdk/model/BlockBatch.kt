b379f953bbf9c7f698a265fe1b34332779a35354
Honza <rychnovsky.honza@gmail.com>
2023-05-08 12:19:48 +0200

    Move BlockBatch to internal models

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/BlockBatch.kt
+++
@@ -1,9 +0,0 @@
-package cash.z.ecc.android.sdk.model
-
-import cash.z.ecc.android.sdk.internal.model.JniBlockMeta
-
-data class BlockBatch(
-    val index: Long,
-    val range: ClosedRange<BlockHeight>,
-    var blocks: List<JniBlockMeta>? = null
-)
