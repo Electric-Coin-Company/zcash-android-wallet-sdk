c18cd620ca70b84dae030099d7203f00bfdbc4e1
Honza <rychnovsky.honza@gmail.com>
2023-05-08 12:14:12 +0200

    CompactBlockRepository documentation update

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/repository/CompactBlockRepository.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/repository/CompactBlockRepository.kt
@@ -12,14 +12,15 @@ interface CompactBlockRepository {
     /**
      * Gets the highest block that is currently stored.
      *
-     * @return the latest block height.
+     * @return the latest block height
      */
     suspend fun getLatestHeight(): BlockHeight?
 
     /**
      * Fetch the compact block for the given height, if it exists.
      *
-     * @return the compact block or null when it did not exist.
+     * @param height of the block we are looking for
+     * @return the compact block summary object or null when it did not exist
      */
     suspend fun findCompactBlock(height: BlockHeight): JniBlockMeta?
 
@@ -33,8 +34,9 @@ interface CompactBlockRepository {
 
     /**
      * This function is supposed to be used continuously while sync process is in progress. It removes all the temporary
-     * block files from the given list of blocks from the device disk.
+     * block files from the given list of blocks.
      *
+     * @param blocks list of compact block summary objects of blocks to delete
      * @return true when all block files from the list are deleted or do not exist, false only if the deletion fails
      */
     suspend fun deleteCompactBlockFiles(blocks: List<JniBlockMeta>): Boolean
@@ -42,8 +44,8 @@ interface CompactBlockRepository {
     /**
      * Write the given flow of blocks to this store, which may be anything from an in-memory cache to a DB.
      *
-     * @param blocks Flow of compact blocks to persist.
-     * @return Flow of number of blocks that were written.
+     * @param blocks Flow of compact blocks to persist
+     * @return list of compact block summary objects of blocks that were written
      */
     suspend fun write(blocks: Flow<CompactBlockUnsafe>): List<JniBlockMeta>
 
