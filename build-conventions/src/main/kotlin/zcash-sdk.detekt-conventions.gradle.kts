dba46c266c1fae278e2ceae21eddc8dddc9ef197
Carter Jernigan <git@carterjernigan.com>
2023-05-31 06:38:42 -0400

    Dependency updates (#1064)
    * [#1060] Detekt 1.23.0
    
    * [#1060] Refactor Detekt integration
    
    Use a convention plugin to be consistent with our other projects.  Also added the Compose Detekt rules.
    
    * [#1061] Ktlint 0.49.1
    
    * [#1012] Emulator.wtf Gradle plugin 0.12.2
    
    * [#1062] Foojay resolver 0.5.0
    
    * [#1055] Remove kapt from darkside-test
    
    * [#1063] Flank 23.04.0

---
+++ build-conventions/src/main/kotlin/zcash-sdk.detekt-conventions.gradle.kts
@@ -0,0 +1,38 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
+plugins {
+    id("io.gitlab.arturbosch.detekt")
+}
+
+dependencies {
+    detektPlugins("io.nlopez.compose.rules:detekt:${project.property("DETEKT_COMPOSE_RULES_VERSION")}")
+}
+
+tasks {
+    register("detektAll", Detekt::class) {
+        parallel = true
+        setSource(files(projectDir))
+        include("**/*.kt")
+        include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+        config.setFrom(files("${rootProject.projectDir}/tools/detekt.yml"))
+        baseline.set(File("${rootProject.projectDir}/tools/detekt-baseline.xml"))
+        buildUponDefaultConfig = true
+    }
+
+    register("detektGenerateBaseline", DetektCreateBaselineTask::class) {
+        description = "Overrides current baseline."
+        buildUponDefaultConfig.set(true)
+        ignoreFailures.set(true)
+        parallel.set(true)
+        setSource(files(rootDir))
+        config.setFrom(files("${rootProject.projectDir}/tools/detekt.yml"))
+        baseline.set(file("$rootDir/tools/detekt-baseline.xml"))
+        include("**/*.kt")
+        include("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/build/**")
+    }
+}
