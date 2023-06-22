4210c99319e41c1ab2dca4bcb5ae46f5db1aa576
Honza <rychnovsky.honza@gmail.com>
2023-06-22 14:45:29 +0200

    [#1088] Demo: Add Missing Compose Previews
    With follow-up filed: Demo: Add Addresses and Transactions Compose Previews #1090

--- demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/transactions/view/Transactions.kt
+++ demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/transactions/view/Transactions.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -22,6 +23,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cash.z.ecc.android.sdk.Synchronizer
 import cash.z.ecc.android.sdk.demoapp.R
@@ -31,15 +33,16 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 
-// @Preview
-// @Composable
-// fun ComposablePreview() {
-//     MaterialTheme {
-//         Addresses()
-//     }
-// }
+@Preview(name = "Transactions")
+@Composable
+private fun ComposablePreview() {
+    MaterialTheme {
+        // TODO [#1090]: Demo: Add Addresses and Transactions Compose Previews
+        // TODO [#1090]: https://github.com/zcash/zcash-android-wallet-sdk/issues/1090
+        // Transactions()
+    }
+}
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Transactions(
     synchronizer: Synchronizer,
