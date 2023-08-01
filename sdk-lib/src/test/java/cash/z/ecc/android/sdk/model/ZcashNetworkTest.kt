673458a24d8c1ca61b136218a40bb0242ac3b82f
Honza <rychnovsky.honza@gmail.com>
2023-08-01 15:39:07 +0200

    [#1133] Set the correct getSubtreeRoots inputs
    - It also better handles setState calls
    - It fixes OpenEndRange -> ClosedRange
    - And it improves logging in syncing mechanism

---
+++ sdk-lib/src/test/java/cash/z/ecc/android/sdk/model/ZcashNetworkTest.kt
@@ -0,0 +1,28 @@
+package cash.z.ecc.android.sdk.model
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ZcashNetworkTest {
+
+    @Test
+    fun is_mainnet_succeed_test() {
+        assertTrue { ZcashNetwork.Mainnet.isMainnet() }
+    }
+
+    @Test
+    fun is_mainnet_fail_test() {
+        assertFalse { ZcashNetwork.Testnet.isMainnet() }
+    }
+
+    @Test
+    fun is_testnet_succeed_test() {
+        assertTrue { ZcashNetwork.Testnet.isTestnet() }
+    }
+
+    @Test
+    fun is_testnet_fail_test() {
+        assertFalse { ZcashNetwork.Mainnet.isTestnet() }
+    }
+}
