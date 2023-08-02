3c31144963206390f49bd34d9b7b1165b4db8ecf
Honza Rychnovský <rychnovsky.honza@gmail.com>
2023-08-02 10:11:04 +0200

    [#1133] Set the correct getSubtreeRoots inputs
    * [#1133] Set the correct getSubtreeRoots inputs
    
    - It also better handles setState calls
    - It fixes OpenEndRange -> ClosedRange
    - And it improves logging in syncing mechanism
    
    * Increase build CI timeout

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/OpenEndRangeExt.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/OpenEndRangeExt.kt
@@ -3,4 +3,4 @@ package cash.z.ecc.android.sdk.internal.ext
 import cash.z.ecc.android.sdk.model.BlockHeight
 
 @OptIn(ExperimentalStdlibApi::class)
-internal fun OpenEndRange<BlockHeight>.toClosedRange(): ClosedRange<BlockHeight> = start..endExclusive
+internal fun OpenEndRange<BlockHeight>.toClosedRange(): ClosedRange<BlockHeight> = start..endExclusive - 1
--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/ScanRange.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/ScanRange.kt
@@ -1,5 +1,6 @@
 package cash.z.ecc.android.sdk.internal.model
 
+import cash.z.ecc.android.sdk.internal.Twig
 import cash.z.ecc.android.sdk.model.BlockHeight
 import cash.z.ecc.android.sdk.model.ZcashNetwork
 
@@ -8,7 +9,13 @@ internal data class ScanRange(
     val range: OpenEndRange<BlockHeight>,
     val priority: Long
 ) {
-    override fun toString() = "ScanRange(range=$range, priority=$priority)"
+    override fun toString() = "ScanRange(range=$range, priority=${getSuggestScanRangePriority()})"
+
+    internal fun getSuggestScanRangePriority(): SuggestScanRangePriority {
+        Twig.verbose { "Current suggested scan range priority: $priority" }
+        return SuggestScanRangePriority.values()
+            .firstOrNull { it.priority == priority } ?: SuggestScanRangePriority.Scanned
+    }
 
     companion object {
         fun new(jni: JniScanRange, zcashNetwork: ZcashNetwork): ScanRange {
@@ -18,4 +25,14 @@ internal data class ScanRange(
             )
         }
     }
+}
+
+@Suppress("MagicNumber")
+internal enum class SuggestScanRangePriority(val priority: Long) {
+    Scanned(10),
+    Historic(20),
+    OpenAdjacent(30),
+    FoundNote(40),
+    ChainTip(50),
+    Verify(60)
 }
