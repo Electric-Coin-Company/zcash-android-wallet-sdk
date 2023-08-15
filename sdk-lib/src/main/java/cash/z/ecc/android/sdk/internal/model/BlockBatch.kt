fd17e7ef0e585be143ca636abb45c11eeda0bf8d
Honza Rychnovský <rychnovsky.honza@gmail.com>
2023-08-15 13:07:38 +0200

    [#1159] Update sync progress reporting
    * [#1168] Checkpoints update
    
    * [#1159] Updated sync progress reporting
    
    * Increase test_robo_demo_app timeout
    
    * Migrate to ClosedEndRange
    
    It’s better to transform suggested ranges from OpenEndRange to ClosedRange as soon as possible to avoid its handling in the rest of the logic.
    
    * Improve all batch count calculating
    
    * Subsequent SbS sync algorithm renaming

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/BlockBatch.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/BlockBatch.kt
@@ -3,9 +3,11 @@ package cash.z.ecc.android.sdk.internal.model
 import cash.z.ecc.android.sdk.model.BlockHeight
 
 internal data class BlockBatch(
-    val order: Long,
+    val inRangeOrder: Long,
+    val crossRangesOrder: Long,
     val range: ClosedRange<BlockHeight>,
     var blocks: List<JniBlockMeta>? = null
 ) {
-    override fun toString() = "BlockBatch(order=$order, range=$range, blocks=${blocks?.size ?: "null"})"
+    override fun toString() = "BlockBatch(crossRangesOrder=$crossRangesOrder, inRangeOrder=$inRangeOrder, " +
+        "range=$range, blocks=${blocks?.size ?: "null"})"
 }
