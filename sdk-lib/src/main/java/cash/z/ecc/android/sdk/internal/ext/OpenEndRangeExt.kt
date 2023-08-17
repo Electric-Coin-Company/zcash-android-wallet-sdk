61f618caf98ce0877e809a371a2b4ef18e6b8c51
Honza <rychnovsky.honza@gmail.com>
2023-08-17 09:03:26 +0200

    Remove OpenEndRange type entirely

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/OpenEndRangeExt.kt
+++
@@ -1,6 +0,0 @@
-package cash.z.ecc.android.sdk.internal.ext
-
-import cash.z.ecc.android.sdk.model.BlockHeight
-
-@OptIn(ExperimentalStdlibApi::class)
-internal fun OpenEndRange<BlockHeight>.toClosedRange(): ClosedRange<BlockHeight> = start..endExclusive - 1
