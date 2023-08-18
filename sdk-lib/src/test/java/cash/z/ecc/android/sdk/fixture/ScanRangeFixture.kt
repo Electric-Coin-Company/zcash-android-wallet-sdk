574b09f064a345fa9dba5a94e675195bb3d6455d
Honza Rychnovský <rychnovsky.honza@gmail.com>
2023-08-18 08:52:02 +0200

    [#1174] Move model classes out of the CompactBlockProcessor
    * [#1174] SbS: Move model classes out of the CompactBlockProcessor
    
    * Move SyncingResult out of the processor

--- sdk-lib/src/test/java/cash/z/ecc/android/sdk/fixture/ScanRangeFixture.kt
+++ sdk-lib/src/test/java/cash/z/ecc/android/sdk/fixture/ScanRangeFixture.kt
@@ -6,7 +6,8 @@ import cash.z.ecc.android.sdk.model.BlockHeight
 import cash.z.ecc.android.sdk.model.ZcashNetwork
 
 object ScanRangeFixture {
-    internal val DEFAULT_CLOSED_RANGE = ZcashNetwork.Testnet.saplingActivationHeight..ZcashNetwork.Testnet.saplingActivationHeight + 9
+    internal val DEFAULT_CLOSED_RANGE =
+        ZcashNetwork.Testnet.saplingActivationHeight..ZcashNetwork.Testnet.saplingActivationHeight + 9
     internal val DEFAULT_PRIORITY = SuggestScanRangePriority.Verify.priority
 
     internal fun new(
