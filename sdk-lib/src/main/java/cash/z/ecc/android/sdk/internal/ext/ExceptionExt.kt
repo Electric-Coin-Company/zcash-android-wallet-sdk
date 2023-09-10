4c8c32587b6968bd64fb38597fb123d7ca0989f2
Honza <rychnovsky.honza@gmail.com>
2023-09-10 10:31:56 +0200

    Add note on scan continuity error texts

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/ExceptionExt.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/ext/ExceptionExt.kt
@@ -45,6 +45,8 @@ internal inline fun <R> tryWarn(
     }
 }
 
+// Note: Do NOT change these texts as they match the ones from ScanError in
+// librustzcash/zcash_client_backend/src/scanning.rs
 internal const val PREV_HASH_MISMATCH = "The parent hash of proposed block does not correspond to the block hash at " +
     "height" // $NON-NLS
 internal const val BLOCK_HEIGHT_DISCONTINUITY = "Block height discontinuity at height" // $NON-NLS
