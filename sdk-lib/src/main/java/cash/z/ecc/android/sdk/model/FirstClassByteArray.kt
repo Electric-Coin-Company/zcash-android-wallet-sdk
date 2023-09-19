905fb1e1a8f0c67a789335dbdc293549a0a49406
Jack Grigg <jack@electriccoin.co>
2023-09-19 18:07:11 +0100

    Improve debug printing of `FirstClassByteArray`

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/FirstClassByteArray.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/FirstClassByteArray.kt
@@ -1,5 +1,7 @@
 package cash.z.ecc.android.sdk.model
 
+import cash.z.ecc.android.sdk.ext.toHex
+
 class FirstClassByteArray(val byteArray: ByteArray) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -13,4 +15,8 @@ class FirstClassByteArray(val byteArray: ByteArray) {
     }
 
     override fun hashCode() = byteArray.contentHashCode()
+
+    override fun toString(): String {
+        return "FirstClassByteArray(%s)".format(byteArray.toHex())
+    }
 }
