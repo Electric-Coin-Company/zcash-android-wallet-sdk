6d51da8b773389146e4e5076b7428b25a34c2954
Honza <rychnovsky.honza@gmail.com>
2023-10-09 11:17:16 +0200

    Fix typo

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/ScanProgress.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/ScanProgress.kt
@@ -9,11 +9,11 @@ internal data class ScanProgress(
     /**
      * Returns progress ratio in [0, 1] range. Any out-of-range value is treated as 0.
      */
-    fun getSafeRatio() = numerator.toFloat().div(denominator).let { ration ->
-        if (ration < 0f || ration > 1f) {
+    fun getSafeRatio() = numerator.toFloat().div(denominator).let { ratio ->
+        if (ratio < 0f || ratio > 1f) {
             0f
         } else {
-            ration
+            ratio
         }
     }
 
