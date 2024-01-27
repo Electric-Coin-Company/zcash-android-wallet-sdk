22407a593de54058a5ba46f0ad47510ab4b09c38
Jack Grigg <jack@electriccoin.co>
2024-01-27 03:26:54 +0000

    Get `WalletSummary` on CBP start and use it to bound subtree roots
    This improves performance in two ways:
    - The `SdkSynchronizer` now reports balances of existing wallets and
      sync progress almost immediately, instead of after the first batch
      of blocks is scanned.
    - In the steady state case, only a few subtree roots are downloaded,
      reducing the time until the first batch of blocks is scanned.
    
    Closes Electric-Coin-Company/zcash-android-wallet-sdk#1310.

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/block/processor/model/GetSubtreeRootsResult.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/block/processor/model/GetSubtreeRootsResult.kt
@@ -6,7 +6,10 @@ import cash.z.ecc.android.sdk.internal.model.SubtreeRoot
  * Internal class for get subtree roots action result.
  */
 internal sealed class GetSubtreeRootsResult {
-    data class SpendBeforeSync(val subTreeRootList: List<SubtreeRoot>) : GetSubtreeRootsResult()
+    data class SpendBeforeSync(
+        val startIndex: UInt,
+        val subTreeRootList: List<SubtreeRoot>
+    ) : GetSubtreeRootsResult()
 
     data object Linear : GetSubtreeRootsResult()
 
