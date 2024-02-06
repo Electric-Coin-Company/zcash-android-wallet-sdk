b361ca1a0891bcfd79edb80b9639a1b0c73a21ab
Honza <rychnovsky.honza@gmail.com>
2024-02-06 16:18:23 +0100

    TestWallet new API adopt

--- darkside-test-lib/src/androidTest/java/cash/z/ecc/android/sdk/darkside/test/TestWallet.kt
+++ darkside-test-lib/src/androidTest/java/cash/z/ecc/android/sdk/darkside/test/TestWallet.kt
@@ -81,7 +81,7 @@ class TestWallet(
     val birthdayHeight get() = synchronizer.latestBirthdayHeight
     val networkName get() = synchronizer.network.networkName
 
-    suspend fun transparentBalance(): WalletBalance {
+    suspend fun transparentBalance(): Zatoshi {
         synchronizer.refreshUtxos(account, synchronizer.latestBirthdayHeight)
         return synchronizer.getTransparentBalance(transparentAddress)
     }
@@ -121,7 +121,7 @@ class TestWallet(
         }
 
         synchronizer.getTransparentBalance(transparentAddress).let { walletBalance ->
-            if (walletBalance.available.value > 0L) {
+            if (walletBalance.value > 0L) {
                 synchronizer.shieldFunds(shieldedSpendingKey)
             }
         }
