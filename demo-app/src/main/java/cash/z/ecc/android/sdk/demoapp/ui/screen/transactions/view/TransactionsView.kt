4a9886e934c36c96ab32295be8aaba397902ec6b
Milan Cerovsky <milan.cerovsky@leeaf.life>
2024-06-04 16:36:48 +0200

    ISSUE-1253 Transaction composable implementation

--- demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/transactions/view/TransactionsView.kt
+++ demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/transactions/view/TransactionsView.kt
@@ -1,18 +1,16 @@
 package cash.z.ecc.android.sdk.demoapp.ui.screen.transactions.view
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Autorenew
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -23,69 +21,91 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cash.z.ecc.android.sdk.SdkSynchronizer
 import cash.z.ecc.android.sdk.Synchronizer
 import cash.z.ecc.android.sdk.demoapp.R
+import cash.z.ecc.android.sdk.demoapp.util.toTransactionState
 import cash.z.ecc.android.sdk.internal.Twig
-import cash.z.ecc.android.sdk.model.TransactionOverview
 import cash.z.ecc.android.sdk.model.WalletAddresses
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 
-@Preview(name = "Transactions")
 @Composable
 @Suppress("ktlint:standard:function-naming")
-private fun ComposablePreview() {
-    MaterialTheme {
-        // TODO [#1090]: Demo: Add Addresses and Transactions Compose Previews
-        // TODO [#1090]: https://github.com/zcash/zcash-android-wallet-sdk/issues/1090
-        // TransactionsView()
+fun Transactions(synchronizer: Synchronizer, onBack: () -> Unit) {
+    val queryScope = rememberCoroutineScope()
+
+    // TODO [#846]: Slow addresses providing
+    // TODO [#846]: https://github.com/zcash/zcash-android-wallet-sdk/issues/846
+    val walletAddresses: WalletAddresses? = flow<WalletAddresses?> {
+        emit(WalletAddresses.new(synchronizer))
+    }.catch { emit(null) }.collectAsState(initial = null).value
+
+    val transactions = if (walletAddresses == null) {
+        emptyList()
+    } else {
+        synchronizer
+            .transactions
+            .collectAsStateWithLifecycle(initialValue = emptyList())
+            .value
+            .map { transactionOverview ->
+                transactionOverview.toTransactionState(
+                    context = LocalContext.current,
+                    onClick = {
+                        queryScope.launch {
+                            synchronizer.getMemos(transactionOverview).toList().run {
+                                Twig.info {
+                                    "Transaction memos: count: $size, contains: ${joinToString().ifEmpty { "-" }}"
+                                }
+                            }
+                        }
+                    }
+                )
+            }
     }
+
+    TransactionsInternal(
+        onBack = onBack,
+        onRefresh = { (synchronizer as SdkSynchronizer).refreshTransactions() },
+        transactions = transactions
+    )
 }
 
 @Composable
-@Suppress("ktlint:standard:function-naming")
-fun Transactions(
-    synchronizer: Synchronizer,
-    onBack: () -> Unit
+private fun TransactionsInternal(
+    onBack: () -> Unit,
+    onRefresh: () -> Unit,
+    transactions: List<TransactionState>
 ) {
     Scaffold(
         topBar = {
             TransactionsTopAppBar(
-                onBack,
-                onRefresh = {
-                    (synchronizer as SdkSynchronizer).refreshTransactions()
-                }
+                onBack = onBack,
+                onRefresh = onRefresh,
             )
         }
     ) { paddingValues ->
-        // TODO [#846]: Slow addresses providing
-        // TODO [#846]: https://github.com/zcash/zcash-android-wallet-sdk/issues/846
-        val walletAddresses =
-            flow {
-                emit(
-                    runCatching {
-                        WalletAddresses.new(synchronizer)
-                    }.onFailure {
-                        Twig.warn { "Wait until the SDK starts providing the addresses" }
-                    }.getOrNull()
-                )
-            }.collectAsState(
-                initial = null
-            ).value
-        if (null != walletAddresses) {
-            TransactionsMainContent(
-                paddingValues = paddingValues,
-                synchronizer,
-                synchronizer.transactions.collectAsStateWithLifecycle(initialValue = emptyList()).value
-                    .toPersistentList()
-            )
+        Column(
+            Modifier.padding(top = paddingValues.calculateTopPadding()),
+        ) {
+            LazyColumn(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+            ) {
+                itemsIndexed(transactions) { index, state ->
+                    Transaction(state)
+                    if (index != transactions.lastIndex) {
+                        HorizontalDivider()
+                    }
+                }
+            }
         }
     }
 }
@@ -120,49 +140,29 @@ private fun TransactionsTopAppBar(
     )
 }
 
+@Preview
 @Composable
 @Suppress("ktlint:standard:function-naming")
-private fun TransactionsMainContent(
-    paddingValues: PaddingValues,
-    synchronizer: Synchronizer,
-    transactions: ImmutableList<TransactionOverview>
-) {
-    val queryScope = rememberCoroutineScope()
-    Column(
-        Modifier
-            .padding(top = paddingValues.calculateTopPadding())
-    ) {
-        LazyColumn(
-            Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            // TODO [#1253]: Let demo-app transaction item show current transaction state
-            // TODO [#1253]: https://github.com/zcash/zcash-android-wallet-sdk/issues/1253
-            items(transactions) { tx ->
-                Button({
-                    val memos = synchronizer.getMemos(tx)
-                    queryScope.launch {
-                        memos.toList().run {
-                            Twig.info {
-                                "Transaction memos: count: $size, contains: ${joinToString().ifEmpty { "-" }}"
-                            }
-                        }
-                    }
-                }) {
-                    val time =
-                        tx.minedHeight?.let {
-                            tx.blockTimeEpochSeconds?.let { kotlinx.datetime.Instant.fromEpochSeconds(it) } ?: "Unknown"
-                        } ?: "Pending"
-                    val value =
-                        if (tx.isSentTransaction) {
-                            -tx.netValue.value
-                        } else {
-                            tx.netValue.value
-                        }
-                    Text("$time, $value")
-                }
-            }
-        }
-    }
+private fun TransactionsPreview() = MaterialTheme {
+    TransactionsInternal(
+        onBack = {},
+        onRefresh = {},
+        transactions = listOf(
+            TransactionState(
+                time = "time",
+                value = "value",
+                fee = "fee",
+                status = "status",
+                onClick = {},
+            ),
+            TransactionState(
+                time = "time",
+                value = "value",
+                fee = "fee",
+                status = "status",
+                onClick = {},
+            ),
+        )
+    )
 }
+
