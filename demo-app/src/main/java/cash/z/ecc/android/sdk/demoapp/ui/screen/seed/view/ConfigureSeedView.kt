12e9b904e71e0eaa2d9d9bd3ff3eb4b6e0d4d75e
Serhii Ihnatiev <17492722+thats-bot@users.noreply.github.com>
2024-06-19 14:13:02 +0200

    [#1236] Add ability to restore wallet from seed phrase and birthday
    * [#1236] Add ability to restore wallet from seed phrase and birthday
    
    - Closes 1236
    - Minor UI improvements
    
    * Remove redundant changelog entry
    
    * Use zcashNetwork.saplingActivationHeight as a fallback value when birthday is empty
    
    * Print validation errors using twig

--- demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/seed/view/ConfigureSeedView.kt
+++ demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/ui/screen/seed/view/ConfigureSeedView.kt
@@ -1,24 +1,40 @@
 package cash.z.ecc.android.sdk.demoapp.ui.screen.seed.view
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cash.z.ecc.android.sdk.WalletInitMode
 import cash.z.ecc.android.sdk.demoapp.R
 import cash.z.ecc.android.sdk.demoapp.ext.defaultForNetwork
 import cash.z.ecc.android.sdk.fixture.WalletFixture
+import cash.z.ecc.android.sdk.internal.Twig
+import cash.z.ecc.android.sdk.model.BlockHeight
 import cash.z.ecc.android.sdk.model.PersistableWallet
 import cash.z.ecc.android.sdk.model.SeedPhrase
 import cash.z.ecc.android.sdk.model.ZcashNetwork
@@ -75,8 +91,10 @@ private fun ConfigureSeedMainContent(
 ) {
     Column(
         Modifier
+            .fillMaxWidth()
             .verticalScroll(rememberScrollState())
             .padding(top = paddingValues.calculateTopPadding())
+            .padding(horizontal = 8.dp)
     ) {
         Button(
             onClick = {
@@ -112,6 +130,91 @@ private fun ConfigureSeedMainContent(
             onClick = onNewWallet
         ) {
             Text(text = stringResource(id = R.string.seed_random))
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        RestoreWalletSection(zcashNetwork, onExistingWallet)
+    }
+}
+
+@Composable
+@Suppress("ktlint:standard:function-naming")
+private fun RestoreWalletSection(
+    zcashNetwork: ZcashNetwork,
+    onExistingWallet: (PersistableWallet) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        val context = LocalContext.current
+
+        var seedPhraseString by rememberSaveable {
+            mutableStateOf("")
+        }
+        var walletBirthdayString by rememberSaveable {
+            mutableStateOf("")
+        }
+        var requiredDataIsEmpty by rememberSaveable {
+            mutableStateOf(true)
+        }
+
+        fun preValidateInput() {
+            requiredDataIsEmpty = seedPhraseString.isEmpty()
+        }
+
+        fun validateAndRestore() {
+            try {
+                val seedPhrase = SeedPhrase.new(seedPhraseString)
+
+                val blockHeight =
+                    walletBirthdayString.toLongOrNull()?.let { blockHeight ->
+                        BlockHeight.new(zcashNetwork, blockHeight)
+                    } ?: zcashNetwork.saplingActivationHeight // fallback to last known valid value
+
+                val wallet =
+                    PersistableWallet(
+                        network = zcashNetwork,
+                        endpoint = LightWalletEndpoint.defaultForNetwork(zcashNetwork),
+                        birthday = blockHeight,
+                        seedPhrase = seedPhrase,
+                        walletInitMode = WalletInitMode.RestoreWallet
+                    )
+                onExistingWallet(wallet)
+            } catch (e: IllegalArgumentException) {
+                val errorMessage = e.message ?: "Unknown error"
+                Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+                Twig.error(e) { errorMessage }
+            }
+        }
+
+        TextField(
+            value = seedPhraseString,
+            onValueChange = {
+                seedPhraseString = it
+                preValidateInput()
+            },
+            minLines = 4,
+            label = { Text(stringResource(id = R.string.seed_custom)) }
+        )
+        TextField(
+            value = walletBirthdayString,
+            onValueChange = {
+                // filter input to contain only digits
+                walletBirthdayString = it.filter { char -> char.isDigit() }
+                preValidateInput()
+            },
+            maxLines = 1,
+            keyboardOptions =
+                KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                ),
+            label = { Text(stringResource(id = R.string.seed_birthday_optional)) }
+        )
+        Button(
+            enabled = !requiredDataIsEmpty,
+            onClick = { validateAndRestore() }
+        ) {
+            Text(text = stringResource(id = R.string.seed_restore))
         }
     }
 }
