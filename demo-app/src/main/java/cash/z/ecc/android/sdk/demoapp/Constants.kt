e8be36a0a70606b5fdbe470027c8c5e871608370
Honza <rychnovsky.honza@gmail.com>
2024-11-25 16:51:48 +0100

    Adopt `getAccounts` in fragment-based Demo app
    We have deprecated the old Fragment-based Demo app. See #973. So, the purpose of these changes is purely the buildability of the new Compose-based Demo app.

--- demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/Constants.kt
+++ demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/Constants.kt
@@ -1,4 +1,4 @@
-package cash.z.ecc.android.sdk.demoapp.ui.common
+package cash.z.ecc.android.sdk.demoapp
 
 import kotlin.time.Duration.Companion.seconds
 
@@ -9,3 +9,5 @@ val ANDROID_STATE_FLOW_TIMEOUT = 5.seconds
  * A tiny weight, useful for spacers to fill an empty space.
  */
 const val MINIMAL_WEIGHT = 0.0001f
+
+const val CURRENT_ACCOUNT = 0
