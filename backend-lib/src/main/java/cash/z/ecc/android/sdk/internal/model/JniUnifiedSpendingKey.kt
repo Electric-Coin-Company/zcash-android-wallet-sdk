15b886fc2717129a29892524b1b59f0a237d202b
Honza <rychnovsky.honza@gmail.com>
2024-11-28 14:11:37 +0100

    Add init to `JniUnifiedSpendingKey.kt`

--- backend-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/JniUnifiedSpendingKey.kt
+++ backend-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/JniUnifiedSpendingKey.kt
@@ -27,6 +27,12 @@ class JniUnifiedSpendingKey(
      */
     val bytes: ByteArray
 ) {
+    init {
+        require(accountUuid.size == 16) {
+            "Account UUID must be 16 bytes"
+        }
+    }
+
     // Override to prevent leaking key to logs
     override fun toString() = "JniUnifiedSpendingKey(account=$account, bytes=***)"
 
