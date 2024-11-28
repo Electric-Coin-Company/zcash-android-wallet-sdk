1ed11c025b2d9b9fcfb91c84bba4601dbb26ebee
Honza <rychnovsky.honza@gmail.com>
2024-11-28 14:12:52 +0100

    File and link followup

--- demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/Constants.kt
+++ demo-app/src/main/java/cash/z/ecc/android/sdk/demoapp/Constants.kt
@@ -10,4 +10,6 @@ val ANDROID_STATE_FLOW_TIMEOUT = 5.seconds
  */
 const val MINIMAL_WEIGHT = 0.0001f
 
-const val CURRENT_ACCOUNT = 0
+// TODO [1644]: Refactor Account ZIP32 index across SDK
+// TODO [1644]: https://github.com/Electric-Coin-Company/zcash-android-wallet-sdk/issues/1644
+const val CURRENT_ZIP_32_ACCOUNT_INDEX = 0
