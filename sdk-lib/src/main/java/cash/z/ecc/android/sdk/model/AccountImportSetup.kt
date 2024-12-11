840df94c71e6d88295ac495699332fcc07d344d5
Honza Rychnovský <honza@rychnovsky.dev>
2024-12-11 15:52:42 +0100

    Update sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/AccountImportSetup.kt
    Co-authored-by: Jack Grigg <jack@electriccoin.co>

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/AccountImportSetup.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/AccountImportSetup.kt
@@ -4,8 +4,11 @@ package cash.z.ecc.android.sdk.model
  * Wrapper for the import account API based on viewing key. Set both [seedFingerprint] and [zip32AccountIndex] null
  * when using [AccountPurpose.ViewOnly].
  *
- * @param accountName A human-readable name for the account
- * @param keySource A string identifier or other metadata describing the source of the seed
+ * @param accountName A human-readable name for the account. This will be visible to the wallet
+ *        user, and the wallet app may obtain it from them.
+ * @param keySource A string identifier or other metadata describing the location of the spending
+ *        key corresponding to the provided UFVK. This should be set internally by the wallet app
+ *        based on its private enumeration of spending methods it supports.
  * @param seedFingerprint The [ZIP 32 seed fingerprint](https://zips.z.cash/zip-0032#seed-fingerprints)
  * @param ufvk The UFVK used to detect transactions involving the account
  * @param zip32AccountIndex The ZIP 32 account-level component of the HD derivation path at which to derive the
