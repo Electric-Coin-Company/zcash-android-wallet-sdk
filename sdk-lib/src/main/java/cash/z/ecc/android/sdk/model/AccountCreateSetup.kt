07f357ed468d51916d60476998b0855b95e2333b
Honza Rychnovský <honza@rychnovsky.dev>
2024-12-11 15:53:10 +0100

    Update sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/AccountCreateSetup.kt
    Co-authored-by: Jack Grigg <jack@electriccoin.co>

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/AccountCreateSetup.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/AccountCreateSetup.kt
@@ -3,8 +3,11 @@ package cash.z.ecc.android.sdk.model
 /**
  * Wrapper for the create account API based on seed phrase
  *
- * @param accountName A human-readable name for the account
- * @param keySource A string identifier or other metadata describing the source of the seed
+ * @param accountName A human-readable name for the account. This will be visible to the wallet
+ *        user, and the wallet app may obtain it from them.
+ * @param keySource A string identifier or other metadata describing the source of the seed. This
+ *        should be set internally by the wallet app based on its private enumeration of spending
+ *        methods it supports.
  * @param seed the wallet's seed phrase. This is required the first time a new wallet is set up. For
  * subsequent calls, seed is only needed if [InitializerException.SeedRequired] is thrown.
  */
