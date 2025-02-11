9b3b280f9294dc49be193dbc4b793a64fc1bf1c2
Jack Grigg <jack@electriccoin.co>
2025-02-11 14:55:57 +1300

    Migrate to latest revision of Zcash crates
    This also (hopefully) fixes a permissions issue with the Tor data dir.

--- backend-lib/src/main/rust/tor.rs
+++ backend-lib/src/main/rust/tor.rs
@@ -14,7 +14,17 @@ impl TorRuntime {
     #[tracing::instrument]
     pub(crate) fn create(tor_dir: &Path) -> anyhow::Result<Self> {
         let runtime = PreferredRuntime::create()?;
-        let client = runtime.block_on(async { Client::create(tor_dir).await })?;
+        let client = runtime.block_on(async {
+            Client::create(
+                tor_dir,
+                // Android apps are run in sandboxes, so we ca rely on them for enforcing
+                // that only the app can access its Tor data.
+                |permissions: &mut fs_mistrust::MistrustBuilder| {
+                    permissions.dangerously_trust_everyone();
+                },
+            )
+            .await
+        })?;
         Ok(Self { runtime, client })
     }
 
