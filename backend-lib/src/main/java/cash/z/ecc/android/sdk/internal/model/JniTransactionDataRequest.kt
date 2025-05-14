c59fab64e0047e7ea3dac67469e02d098d73c127
Jack Grigg <jack@electriccoin.co>
2025-05-14 17:31:19 +0100

    Return all `TransactionDataRequest` details across JNI

--- backend-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/JniTransactionDataRequest.kt
+++ backend-lib/src/main/java/cash/z/ecc/android/sdk/internal/model/JniTransactionDataRequest.kt
@@ -19,10 +19,13 @@ sealed class JniTransactionDataRequest {
     ) : JniTransactionDataRequest()
 
     @Keep
-    data class SpendsFromAddress(
+    data class TransactionsInvolvingAddress(
         val address: String,
         val startHeight: Long,
         val endHeight: Long,
+        val requestAt: Long,
+        val txStatusFilter: JniTransactionStatusFilter,
+        val outputStatusFilter: JniOutputStatusFilter,
     ) : JniTransactionDataRequest() {
         init {
             // We require some of the parameters below to be in the range of unsigned integer, because of the Rust layer
@@ -39,6 +42,39 @@ sealed class JniTransactionDataRequest {
                     "End height $endHeight must be greater than start height $startHeight."
                 }
             }
+            // We use -1L to represent None across JNI.
+            if (requestAt != -1L) {
+                require(requestAt >= 0) {
+                    "requestAt $requestAt is outside of allowed timestamp range"
+                }
+            }
         }
     }
+}
+
+/**
+ * Serves as cross layer (Kotlin, Rust) communication class.
+ */
+@Keep
+sealed class JniTransactionStatusFilter {
+    @Keep
+    data object Mined : JniTransactionStatusFilter()
+
+    @Keep
+    data object Mempool : JniTransactionStatusFilter()
+
+    @Keep
+    data object All : JniTransactionStatusFilter()
+}
+
+/**
+ * Serves as cross layer (Kotlin, Rust) communication class.
+ */
+@Keep
+sealed class JniOutputStatusFilter {
+    @Keep
+    data object Unspent : JniOutputStatusFilter()
+
+    @Keep
+    data object All : JniOutputStatusFilter()
 }
