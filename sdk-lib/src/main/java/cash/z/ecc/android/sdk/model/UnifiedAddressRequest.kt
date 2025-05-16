dfbbf94ee13d92bdb160507f971d0e0cdd45ed1a
Milan Cerovsky <milan@z.cash>
2025-05-16 17:55:57 +0200

    UnifiedAddressRequest refactor

--- sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/UnifiedAddressRequest.kt
+++ sdk-lib/src/main/java/cash/z/ecc/android/sdk/model/UnifiedAddressRequest.kt
@@ -1,26 +1,31 @@
 package cash.z.ecc.android.sdk.model
 
-enum class UnifiedAddressRequest(
+sealed interface UnifiedAddressRequest {
+
     val flags: Int
-) {
-    P2PKH(FLAG_P2PKH),
-    SAPLING(FLAG_SAPLING),
-    ORCHARD(FLAG_ORCHARD),
-    ALL(FLAG_ALL),
-    SHIELDED(FLAG_SHIELDED)
+
+    /**
+     * The requested address can receive transparent p2pkh outputs.
+     */
+    data object P2PKH : UnifiedAddressRequest { override val flags: Int = 1 } // 0b00000001
+
+    /**
+     * The requested address can receive Sapling outputs.
+     */
+    data object Sapling : UnifiedAddressRequest { override val flags: Int = 4 } // 0b00000100
+
+    /**
+     * The requested address can receive Orchard outputs.
+     */
+    data object Orchard : UnifiedAddressRequest { override val flags: Int = 8 } // 0b00001000
+
+    private data class Custom(override val flags: Int) : UnifiedAddressRequest
+
+    infix fun or(other: UnifiedAddressRequest): UnifiedAddressRequest = Custom(flags or other.flags)
+
+    companion object {
+        val all = P2PKH or Sapling or Orchard
+
+        val shielded = Sapling or Orchard
+    }
 }
-
-// / The requested address can receive transparent p2pkh outputs.
-private const val FLAG_P2PKH = 1 // 0b00000001
-
-// / The requested address can receive Sapling outputs.
-private const val FLAG_SAPLING = 4 // 0b00000100
-
-// / The requested address can receive Orchard outputs.
-private const val FLAG_ORCHARD = 8 // 0b00001000
-
-// / The requested address can receive any supported type of output
-private const val FLAG_ALL = FLAG_P2PKH or FLAG_SAPLING or FLAG_ORCHARD
-
-// / The requested address is shielded-only;
-private const val FLAG_SHIELDED = FLAG_SAPLING or FLAG_ORCHARD
